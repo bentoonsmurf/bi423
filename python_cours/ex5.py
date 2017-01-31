2533f889aaf6b35c5543ed61d23787bcdde0f75e
user <you@example.com>
2017-01-31 18:25:33 +0100

    ex 6

--- python_cours/ex5.py
+++ python_cours/ex5.py
@@ -1,5 +1,5 @@
 
-
+#  int(vari)
 
 
 i=input("entrer un nombre  ")
---
+++ python_cours/ex6.py
@@ -0,0 +1,13 @@
+
+
+ch=raw_input("entrer une chaine de char ")
+i=input("un nombre ")
+
+if len(ch)>i:
+
+	print ch[0:i]
+else:
+	print " trop peut de char "
+
+
+
