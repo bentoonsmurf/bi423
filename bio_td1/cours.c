be8b016f7d4dde87996b098fee913d8df739bdc7
user <you@example.com>
2017-03-14 16:35:43 +0100

    dd

---
+++ bio_td1/cours.c
@@ -0,0 +1,40 @@
+typedef double simple ;typedef double sequance;typedef sequance sequances;typedef double fragment;
+la complexité = la quantité de suite sequantiel #kamère
+suites nucleotidiques distinctes donc dificile a reduire
+la denaturation = l'adn' est coupé en deux fils
+un double brun absorbe bien plus que un simple brum a 260 nm
+
+
+// questions d' exo 1
+/// c'est une coubre synetique
+/// l'absorbance sur le max
+
+1 C0 represente un concentration
+2 afin de permetre a l''experience de se faire
+  c pour les faire se chercher 
+  suivant la complexité du genome certainvons trouver 
+  plus facilement un partenaire que les autres
+  donc certaines sequances vons se renaturé plus vite que d'autres
+3 kamere
+4 refroidissement rapide l'adn' pour pas que tout soit renaturé
+  la courbe Ec resemble a ca car chaque fragment est differents
+  pour la souris il y a au moins 3 grand groupes de fragment 10%
+  le premier groupe est composé de sequances hautement répété 25% 
+  le dernier 60%
+5 l'adn' de la souris est 3 log plus grand que celui de Ec
+donc Ec = 5x10^8
+	souris= 5x10^9 ---> 10% ultra reppeté =5x10^8
+
+// question d' exo2
+
+1 il n''on pas ete decoupé car ils on la meme forme
+2 si les bacteries se reasembles plus vite c parce que elles on 
+  des sequances similaires =30 % sont fortement homologue
+  il y avais 10 bacteries ( 4 log de plus que Ec)
+
+
+
+
+
+
+mugen
